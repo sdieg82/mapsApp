3413009caa37b04bf5d8ee836dab0a31b65901d5
Diego <sdieg82@gmail.com>
2024-09-18 12:45:13 -0500

    token maps and fullscreen

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name:string
+  route:string,
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
-
+  public menuItem:MenuItem[]=[
+    {route:'/maps/fullscreen',name:'FullScreen'},
+    {route:'/maps/zoom-range',name:'ZoomRange'},
+    {route:'/maps/markers',name:'Markers'},
+    {route:'/maps/properties',name:'Houses'},
+  ]
 }
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
 
+  constructor() {
+    // Asignar el token en el constructor
+    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoic2RpZWc4MiIsImEiOiJjbTE4MXozZnUwenlhMnFwczI1ZGhjZHljIn0.HPMbk532neYWTj-1VQAy4Q';
+  }
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
