c9519d7b48861f9048d65857608db276cfb2bbb1
Diego <sdieg82@gmail.com>
2024-09-18 10:19:28 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [
     provideClientHydration()
--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MapsRoutingModule } from './maps-routing.module';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 
 @NgModule({
@@ -9,5 +16,14 @@ import { MapsRoutingModule } from './maps-routing.module';
         MapsRoutingModule
     ],
     exports: [],
+    declarations: [
+      SideMenuComponent,
+      MiniMapComponent,
+      MapsLayoutComponent,
+      FullScreenPageComponent,
+      MarkersPageComponent,
+      PropertiesPageComponent,
+      ZoomRangePageComponent
+    ],
 })
 export class MapsModule { }
